064b0ed6d628f0a9fdb86ab5ca50e13d6b966310
Kane Grills <kane@plustime.com.au>
2023-10-11 15:05:41 +1000

    build: 0.0.10 Add vue plugin

--- sandbox/src/main.ts
+++ sandbox/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import vuetify from './vuetify'
+import HistoirePlugin from '../../src/plugin'
 
 import App from './App.vue'
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).use(HistoirePlugin).mount('#app')
